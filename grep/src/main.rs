2f8360335d0252029841088a8967931f14a8aa21
Andrew X. Shah <drewshah0@gmail.com>
2023-08-26 18:42:13 -0600

    fix(grep): resolve name

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,14 +1,14 @@
 use std::{env, process};
 
 fn main() {
-    let config = xgrep::Config::build(env::args()).unwrap_or_else(|err| {
+    let config = grep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Failed to parsed arguments: {err}");
         process::exit(1);
     });
     println!("query: {}", config.query);
     println!("path: {}", config.file_path);
 
-    if let Err(e) = xgrep::run(config) {
+    if let Err(e) = grep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
